41c2103df2477fcb63b7a5ac55a0a1573829128f
tony <tony.li@vechain.com>
2019-10-21 22:43:36 +0800

    add foundation integrity script

---
+++ src/scripts/foundtaion-integrity.ts
@@ -0,0 +1,38 @@
+import { initConnection } from '../db'
+import { Persist } from '../foundation/persist'
+import { getConnection } from 'typeorm'
+import { Block } from '../db/entity/block'
+import { displayID } from '../utils'
+
+const STOP_NUMBER = 0
+const persist = new Persist()
+const getBlock = (id: string) => {
+    return getConnection()
+        .getRepository(Block)
+        .findOne({ id })
+}
+
+initConnection().then(async (conn) => {
+    const head = await persist.getHead()
+
+    let current = await getBlock(head.value)
+
+    for (; ;) {
+        const b = await getBlock(current.parentID)
+        if (b === null) {
+            throw new Error(`continuity Block(${displayID(current.id)})'s parentID(${current.parentID}) missing`)
+        }
+        if (b.number === STOP_NUMBER) {
+            console.log('Finished integrity check')
+            break
+        }
+        if (b.number % 1000 === 0) {
+            console.log(`Processed to Block(${displayID(b.id)})`)
+        }
+        current = b
+    }
+
+}).catch(e => {
+    console.log('Integrity check: ')
+    console.log(e)
+})
