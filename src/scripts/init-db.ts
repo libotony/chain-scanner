95c0d813828d01e4629d6b3c0e84406c9f5428db
tony <tony.li@vechain.com>
2019-11-20 19:40:08 +0800

    update init script

--- src/scripts/init-db.ts
+++ src/scripts/init-db.ts
@@ -2,9 +2,13 @@ import { getConnectionOptions, createConnection } from 'typeorm'
 
 Promise.resolve().then(async () => {
    const opt = await getConnectionOptions()
-   createConnection(Object.assign({}, opt, {
+   await createConnection(Object.assign({}, opt, {
       synchronize: true,
       logging: true,
       logger: undefined
    }))
-}).catch(console.log)
+   process.exit(0)
+}).catch((e) => {
+   console.log(e)
+   process.exit(-1)
+})
