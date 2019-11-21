b4c62318a8f759ab1f6e0d11643dbd41946907aa
tony <tony.li@vechain.com>
2019-11-21 19:46:51 +0800

    update explorer db

--- src/utils.ts
+++ src/utils.ts
@@ -22,6 +22,24 @@ export const displayID = (blockID: string) => {
     return `${blockIDtoNum(blockID)}...${blockID.slice(58)}`
 }
 
+export const sanitizeHex = (val: string) => {
+    if (val.startsWith('0x')) {
+        val = val.slice(2)
+    }
+    if (val.length % 2) {
+        val = '0' + val
+    }
+    return val
+}
+
+export const hexToBuffer = (val: string) => {
+    if ( !/^0x[0-9a-fA-f]+/i.test(val)) {
+        throw new Error('hex string required as param but got: ' + val)
+    }
+
+    return Buffer.from(sanitizeHex(val), 'hex')
+}
+
 export class InterruptedError extends Error {
     constructor() {
         super('interrupted')
