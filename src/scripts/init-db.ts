587520fe4e2e5734cc02acb71eb332cb3c07956a
tony <liboliqi@gmail.com>
2020-02-21 13:13:03 +0800

    init db by migration: enable table compression

--- src/scripts/init-db.ts
+++ src/scripts/init-db.ts
@@ -2,6 +2,7 @@ import { getConnectionOptions, createConnection } from 'typeorm'
 
 Promise.resolve().then(async () => {
    const opt = await getConnectionOptions()
+   console.warn('This file should not be used to create tables, use migrations instead!')
    await createConnection(Object.assign({}, opt, {
       synchronize: true,
       logging: true,
