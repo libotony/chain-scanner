0571cae53d7c80ef0b07d4471e713406ebafcaa8
tony <liboliqi@gmail.com>
2021-11-29 17:12:22 +0800

    update db & migrations

--- src/migrations/1637304637810-counts.ts
+++ src/migrations/1637304637810-counts.ts
@@ -4,7 +4,7 @@ export class counts1637304637810 implements MigrationInterface {
     name = 'counts1637304637810'
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("CREATE TABLE `counts` (`address` binary(20) NOT NULL, `type` int NOT NULL, `in` int UNSIGNED NOT NULL, `out` int UNSIGNED NOT NULL, `self` int UNSIGNED NOT NULL, PRIMARY KEY (`address`, `type`)) ENGINE=InnoDB", undefined);
+        await queryRunner.query("CREATE TABLE `counts` (`address` binary(21) NOT NULL, `type` int NOT NULL, `in` int UNSIGNED NOT NULL, `out` int UNSIGNED NOT NULL, `self` int UNSIGNED NOT NULL, PRIMARY KEY (`address`, `type`)) ENGINE=InnoDB", undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
