e8836844edb33aff5a49df74bcad9c0bad78fde1
tony <liboliqi@gmail.com>
2021-12-01 13:46:31 +0800

    fix log time converting

--- src/reporter.ts
+++ src/reporter.ts
@@ -32,10 +32,10 @@ export class Reporter {
         const elapsed = hrtime.bigint() - this.ts
         
         let time = ''
-        if (elapsed > BigInt(1e6)) {
-            time = (Number(elapsed)/1e6).toFixed(2) +'s'
+        if (elapsed > BigInt(1e9)) {
+            time = (Number(elapsed)/1e9).toFixed(2) +'s'
         } else {
-            time = (Number(elapsed)/1e3).toFixed(2) +'ms'
+            time = (Number(elapsed)/1e6).toFixed(2) +'ms'
         }
         const ret = `imported blocks(${this.endBlock - this.startBlock}) at Block(${this.endBlock}), time: ${time}`
 
